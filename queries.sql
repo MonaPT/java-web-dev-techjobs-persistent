20414a395b42c2e92f26a651eac9bc8dd59efa67
Mona <mona.ptn01@gmail.com>
2020-05-04 09:44:26 -0500

    Assignment 4 commit

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 ## Part 1: Test it with SQL
-
+desc job
 ## Part 2: Test it with SQL
-
+Select * from employers where location='St. Louis'
 ## Part 3: Test it with SQL
-
+drop table job
 ## Part 4: Test it with SQL
